3f5ab97a6e76661463a3c1fba9f61883d50962d5
Chlebekk <bar1walc@gmail.com>
2023-09-12 11:44:45 +0200

    Abstract class

--- IdeaProjects/b/SchoolObject/src/Main.java
+++ IdeaProjects/b/SchoolObject/src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        Person Person1=new Person("Jan","Mama",230957092);
-        System.out.println(Person1.getName()+","+Person1.getSurname()+","+Person1.getAge());
-        Person Person2 =new Person("Ola","Nowak");
-        System.out.println(Person2);
+        //Person Person1=new Person("Jan","Mama",230957092);
+        //System.out.println(Person1.getName()+","+Person1.getSurname()+","+Person1.getAge());
+        //Person Person2 =new Person("Ola","Nowak");
+        //System.out.println(Person2);
         Student student1P1= new Student("Maksiu","Golgowski",3);
         Student student1P2 = new Student("NieMaksiu","NieGolgowski",23);
         System.out.println(student1P1);
