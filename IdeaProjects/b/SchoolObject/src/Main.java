48c6ed4e77d34b1d5b2158edcb55425d74e2301e
Chlebekk <bar1walc@gmail.com>
2023-09-12 11:18:28 +0200

    values private, getters, setters

--- IdeaProjects/b/SchoolObject/src/Main.java
+++ IdeaProjects/b/SchoolObject/src/Main.java
@@ -1,8 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         Person Person1=new Person();
-        Person1.name="Jan";
-        System.out.println(Person1.name);
+        Person1.setName("Jan");
+        Person1.setSurname("Mama");
+        Person1.setAge(420);
+        System.out.println(Person1.getName()+","+Person1.getSurname()+","+Person1.getAge());
 
     }
 
--- IdeaProjects/b/SchoolObject/src/Person.java
+++ IdeaProjects/b/SchoolObject/src/Person.java
@@ -1,5 +1,37 @@
 public class Person {
-    public String name;
-    public String surname;
-    public int age;
+    private String name;
+    private String surname;
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        if (name.length()<3)
+        {System.out.println("Name must be at least 3 characters");}
+        else{this.name = name;}
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        if (surname.length()<3)
+        {System.out.println("Surname must be at least 3 characters");}
+        else{this.surname = surname;}
+
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        if (age<0){
+            this.age=0;
+        }else{this.age = age;}
+
+    }
 }
